8caea880af7e7751bc73bc97645fbc06f68a6956
iosdevlog <iosdevlog@iosdevlog.com>
2018-03-26 14:17:23 +0800

    range slider init

---
+++ code/010. CustomSlider/CustomSlider/RangeSlider.h
@@ -0,0 +1,18 @@
+//
+//  RangeSlider.h
+//  CustomSlider
+//
+//  Created by iOS Dev Log on 2018/3/26.
+//  Copyright © 2018年 iOSDevLog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RangeSlider : UIControl
+
+@property (nonatomic) float maximumValue;
+@property (nonatomic) float minimumValue;
+@property (nonatomic) float upperValue;
+@property (nonatomic) float lowerValue;
+
+@end
